383984a8a9ac30e1c9321f837c24ac2729907623
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2023-04-30 20:18:10 -0700

    Zombie processes

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * infinite_white - infinite loop
+ *
+ * Return: 0
+ */
+
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+
+/**
+ * main - entry poin
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	pid_t pd;
+
+	for (i = 0; i < 5; i++)
+	{
+		pd = fork();
+
+		if (pd == 0)
+			exit(0);
+		else
+			printf("Zombie process created, PID: ZOMBIE_PID: %d\n", pf);
+	}
+	infinite_while();
+	return (0);
+}
