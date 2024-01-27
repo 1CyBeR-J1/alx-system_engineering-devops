9d5fffa901f732febef4151555e996f1f7267833
1CyBeR-J1 <marvellousojeleke@gmail.com>
2024-01-26 19:44:42 -0800

    return info on empID

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+scripts that uses REST API for a given employee ID, returns information
+about his/her TODO list progress.
+"""
+
+import requests
+import sys
+
+if __name__ '__main__':
+    empId = int(sys.argv[1])
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    url = base_url + "/" + empId
+
+    response = requests.get(url)
+    empName = response.json().get('name')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
+
+    for task in tasks:
+        if task.get('completed'):
+            done_tasks.append(task)
+            done += 1
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(empName, done, len(tasks)))
+
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
