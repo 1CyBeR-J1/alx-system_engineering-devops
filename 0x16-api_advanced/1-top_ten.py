3d1b2150d701c327b3a762ad2e75747eff993b81
1CyBeR-J1 <marvellousojeleke@gmail.com>
2024-01-27 13:25:00 -0800

    Erro Fixed

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,21 +12,18 @@ def top_ten(subreddit):
     titles of the first 10 hot posts listed for a given subreddit.
     """
 
-    if subreddit is None or not isinstance(subreddit, str):
+    url_base = 'http://www.reddit.com/r/'
+    url_query = '{:s}/hot.json?limit={:d}'.format(subreddit, 10)
+    headers = {'user-agent': 'Botquest'}
+    r = requests.get(url_base + url_query, headers=headers)
+
+    if (r.status_code is 302):
         print("None")
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    params = {'limit': 10}
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-
-    response = get(url, headers=user_agent, params=params)
-    results = response.json()
-
-    try:
-        my_data = results.get('data').get('children')
-
-        for i in my_data:
-            print(i.get('data').get('title'))
-
-    except Exception:
+        return
+    if (r.status_code is 404):
         print("None")
+        return
+    else:
+        r = r.json()
+        for post in r['data']['children']:
+            print(post['data']['title'])
