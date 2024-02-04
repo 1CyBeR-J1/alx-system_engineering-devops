9e1071512a4a15f51f69bc0f98cd0b1fb29945bb
1CyBeR-J1 <marvellousojeleke@gmail.com>
2024-02-03 17:18:22 -0800

    Error Fixed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,14 +13,16 @@ def number_of_subscribers(subreddit):
     for a given subreddit
     """
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) Apple' +
-        'WebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
-    }
-    r = requests.get('https://www.reddit.com/r/{:}/about.json'.format(
-        subreddit), headers=headers, allow_redirects=False)
-    if r.status_code >= 300:
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
-    json = r.json()
-    data_dict = json.get('data')
-    return (data_dict.get('subscribers'))
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
+        return 0
